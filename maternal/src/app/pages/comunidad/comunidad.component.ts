fa5e676df4726abd0f93b1c2f98bb6934a4af01d
carolaljime21 <carolalvaradojime@gmail.com>
2024-07-16 06:16:58 -0500

    cambios

--- maternal/src/app/pages/comunidad/comunidad.component.ts
+++ maternal/src/app/pages/comunidad/comunidad.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-comunidad',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, FooterComponent, RouterLink, CommonModule],
   templateUrl: './comunidad.component.html',
   styleUrl: './comunidad.component.css'
 })
