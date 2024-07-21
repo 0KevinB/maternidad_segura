08eb412bb4ca2edf4a770b2c7759eebbec682a9b
0KevinB <kabarrazueta@utpl.edu.ec>
2024-07-20 23:16:00 -0500

    Cambios

--- server/dist/models/db.js
+++ server/dist/models/db.js
@@ -4,17 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mysql = require('mysql2');
-/* const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    //password: '123456789',
-    password: '',
-    database: 'sistema_medico',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-export default connection */
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const connection = mysql.createConnection({
--- server/src/models/db.ts
+++ server/src/models/db.ts
@@ -1,17 +1,5 @@
 const mysql = require('mysql2');
 
-/* const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    //password: '123456789',
-    password: '',
-    database: 'sistema_medico',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-export default connection */
-
 import dotenv from 'dotenv';
 
 dotenv.config();
